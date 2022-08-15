5b85465c8af93be18085f7a6aa286dfd14823f84
zerom-H <cupdisin@gmail.com>
2022-08-15 22:22:51 +0900

    JpaRepository add

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -32,4 +32,33 @@ class MemberRepositoryTest {
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         Assertions.assertThat(findMember).isEqualTo(member);
     }
+
+
+    //Optional 로 할 떄는 or else? 이런걸로 하는게 좋다함..
+
+    @Test
+    void basicCRUD() {
+        Member memberA = new Member("memberA");
+        Member memberB = new Member("memberB");
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        //단건 조회
+        Member findMemberA = memberRepository.findById(memberA.getId()).get();
+        Member findMemberB = memberRepository.findById(memberB.getId()).get();
+        Assertions.assertThat(findMemberA).isEqualTo(memberA);
+        Assertions.assertThat(findMemberB).isEqualTo(memberB);
+
+        //카운터 검증
+        long count = memberRepository.count();
+        Assertions.assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(memberA);
+        memberRepository.delete(memberB);
+
+        long deletedCount = memberRepository.count();
+
+        Assertions.assertThat(deletedCount).isEqualTo(0);
+    }
 }
